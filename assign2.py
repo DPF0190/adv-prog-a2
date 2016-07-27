f9ebc831ed9a9726818ca49161d9f725cdad95e1
DPF0190 <domenick.fumai@gmail.com>
2016-07-27 18:27:21 +0000

    assign 2

--- assign2.py
+++ assign2.py
@@ -8,21 +8,22 @@ class DebuggerTest(object):
         self.func = func
 
     def __call__ (self, *args, **kwargs):
-        print 'my function name is: %s' % self.func.func_name
+        print 'my function name is: %s *args %r' % (self.func.func_name, args)
         _ret = self.func(*args, **kwargs)
 
-        print 'my function name also is: %s' % self.func.func_name
+        print 'my return value is: %s *args %r' % (self.func.func_name, args)
+
         return _ret
 
 @DebuggerTest
 def avgPrice(arg1, arg2):
-    print 'Avg price is 1st purch price %r being averaged by 2nd purch price %r: %r' % (arg1, arg2, (arg1 + arg2)/2)
+    print 'avgPrice is 1st purch price %r being averaged by 2nd purch price %r: %r' % (arg1, arg2, (arg1 + arg2)/2)
     return ((arg1 + arg2)/2)
 
 
 @DebuggerTest
 def Total_traded(arg1, arg2):
-    print 'Total traded is price %r times shares %r: %r is total dollar amount traded' % (arg1, arg2, arg1 * arg2)
+    print 'Total_traded is price %r times shares %r: %r is total dollar amount traded' % (arg1, arg2, arg1 * arg2)
     return arg1 * arg2
 
 logging.basicConfig(filename='example.log',level=logging.DEBUG)
--- assign2run.py
+++ assign2run.py
@@ -3,6 +3,6 @@ from assign2 import avgPrice, Total_traded
 avgPrice = avgPrice(20, 30)
 Total_traded = Total_traded(30, 10)
 
-print 'My avg price is: %r. My total traded is: %r' % (avgPrice, Total_traded)
+print 'avgPrice: %r. Total_traded: %r' % (avgPrice, Total_traded)
 
 
